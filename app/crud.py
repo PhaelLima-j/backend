5d2a50b52988f1580f5ba0c1904d3c45ed82c112
PhaelLima-j <raphael.rodrigues.58010@a.fecaf.com.br>
2026-06-26 15:46:24 -0300

    [ADD] CRUD de filmes

--- app/crud.py
+++ app/crud.py
@@ -13,3 +13,21 @@ def create_movie(db: Session, movie: schemas.MovieCreate):
     db.commit()
     db.refresh(db_movie)
     return db_movie
+
+def update_movie(db: Session, movie_id: int, movie: schemas.MovieCreate):
+    db_movie = db.query(models.Movie).filter(models.Movie.id == movie_id).first()
+    if not db_movie:
+        return None
+    for key, value in movie.model_dump().items():
+        setattr(db_movie, key, value)
+    db.commit()
+    db.refresh(db_movie)
+    return db_movie
+
+def delete_movie(db: Session, movie_id: int):
+    db_movie = db.query(models.Movie).filter(models.Movie.id == movie_id).first()
+    if not db_movie:
+        return None
+    db.delete(db_movie)
+    db.commit()
+    return db_movie
